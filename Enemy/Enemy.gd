ea4aa3144e05c97e0b135f32909e60c84c0b06c3
Nefty <jeffreyfkoopman@gmail.com>
2023-12-29 22:36:43 -0500

    progress :)

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,11 +1,30 @@
 extends PathFollow3D
 
 @export var speed: float = 5.0
+@export var max_health: int = 50
+@export var gold_value: int = 15
 
+var current_health: int:
+	set(health_in):
+		if health_in < current_health:
+			animation_player.play("TakeDamage")
+		current_health = health_in
+		if current_health < 1:
+			bank.gold += gold_value
+			queue_free()
+	get:
+		return current_health
+		
+@onready var bank: MarginContainer = get_tree().get_first_node_in_group("bank")
 @onready var base = get_tree().get_first_node_in_group("base")
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	current_health = max_health;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress += delta * speed
 	if progress_ratio == 1.0:
 		base.take_damage()
+		set_process(false)
---
+++ Turret/turret.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+@export var projectile: PackedScene
+@export var turret_range: float = 10.0
+
+var enemy_path: Path3D
+var target: PathFollow3D
+
+@onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	target = find_best_target()
+	if target != null:
+		look_at(target.global_position, Vector3.UP, true)
+
+func _on_timer_timeout() -> void:
+	if target != null:
+		var shot = projectile.instantiate()
+		add_child(shot)
+		shot.global_position = turret_top.global_position
+		shot.direction = global_transform.basis.z
+		animation_player.play("Fire")
+	
+func find_best_target() -> PathFollow3D:
+	var best_target: PathFollow3D = null
+	var best_progress: int = 0
+	
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			if global_position.distance_to(enemy.global_position) < turret_range:
+				if enemy.progress > best_progress:
+					best_target = enemy
+					best_progress = enemy.progress
+	return best_target
+	
+	
+	
+	
+	
+	
+	
+	
+	
