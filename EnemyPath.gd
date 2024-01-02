70352856346a056450bddbd4d98e3bb29b8fe48d
Nefty <jeffreyfkoopman@gmail.com>
2024-01-02 14:26:46 -0500

    more progress :)

--- EnemyPath.gd
+++ EnemyPath.gd
@@ -1,7 +1,15 @@
 extends Path3D
 
 @export var enemy_scene: PackedScene
+@export var difficulty_manager: Node
+
+@onready var enemy_spawn_timer: Timer = $EnemySpawnTimer
 
 func spawn_enemy() -> void:
 	var new_enemy = enemy_scene.instantiate()
+	new_enemy.max_health = difficulty_manager.get_enemy_health()
 	add_child(new_enemy)
+	enemy_spawn_timer.wait_time = difficulty_manager.get_spawn_time()
+
+func _on_difficulty_manager_stop_spawning_enemies() -> void:
+	enemy_spawn_timer.stop()
